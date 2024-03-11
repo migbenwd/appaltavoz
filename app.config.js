89cc269f5817d840d55cc7d6f17014a2af8582a6
migben.com <miguelangel.benitezyanez@gmail.com>
2024-03-11 16:57:36 -0400

    eas update

--- app.config.js
+++ app.config.js
@@ -1,32 +1,37 @@
 export default {
-    name: "newsapp",
-    slug: "newsapp",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-        image: "./assets/splash.png",
-        resizeMode: "contain",
-        backgroundColor: "#ffffff"
+  name: 'newsapp',
+  slug: 'newsapp',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.migben.newsapp',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
     },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-        supportsTablet: true,
-        bundleIdentifier: "com.migben.newsapp"
+    package: 'com.migben.newsapp',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {
+    eas: {
+      projectId: 'f8c5cb62-7c08-44b4-a3c9-f46d2857f584',
     },
-    android: {
-        adaptiveIcon: {
-            foregroundImage: "./assets/adaptive-icon.png",
-            backgroundColor: "#ffffff"
-        },
-        package: "com.migben.newsapp"
-    },
-    web: {
-        favicon: "./assets/favicon.png"
-    },
-	
-    /*
+  },
+
+  /*
 	extra: {
         eas: {
             projectId: "ea85bda6-b26d-486a-90f0-5b8abbc6cf22"
