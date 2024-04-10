57c124e1a98c37a532837d7f7b62e9909997749f
migben.com <miguelangel.benitezyanez@gmail.com>
2024-04-10 09:39:53 -0400

    a PRODUCTION - cambio de name en SLUG

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
   name: 'Noticiero Altavoz',
-  slug: 'noticieroaltavoz',
+  slug: 'noticieroaltavozapp',
   version: '1.1.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
@@ -13,22 +13,22 @@ export default {
   assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: 'com.noticierolatavoz.appnoticieroaltavoz',
+    bundleIdentifier: 'com.noticierolatavoz.noticieroaltavozapp',
   },
   android: {
     adaptiveIcon: {
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
-    package: 'com.noticierolatavoz.appnoticieroaltavoz',
+    package: 'com.noticierolatavoz.noticieroaltavozapp',
   },
   web: {
     favicon: './assets/favicon.png',
   },
 
-  extra: {
-    eas: {
-      projectId: '0b48ef1f-11de-4b09-a0ea-95afe2165886',
-    },
-  },
+  // extra: {
+  //   eas: {
+  //     projectId: '0b48ef1f-11de-4b09-a0ea-95afe2165886',
+  //   },
+  // },
 };
