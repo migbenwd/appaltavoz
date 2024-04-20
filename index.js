b51749d52a3121fd1773f7cbb437ba4c370dab76
migben.com <miguelangel.benitezyanez@gmail.com>
2024-04-20 16:01:45 -0400

    cambios en index

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import { registerRootComponent } from 'expo';
+// import { registerRootComponent } from 'expo';
+// import TrackPlayer from 'react-native-track-player';
+// import App from './App';
+
+// registerRootComponent(App);
+// TrackPlayer.registerPlaybackService(() => require('./service'));
+
+import { AppRegistry } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './App';
+import { name as appName } from './app.json';
+import { playbackService } from './trackPlayerServices';
 
-registerRootComponent(App);
-TrackPlayer.registerPlaybackService(() => require('./service'));
+AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
